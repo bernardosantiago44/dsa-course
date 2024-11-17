5b5e1d6cda3acce7d1717a436feca68804cd1549
bernardosantiago44 <bernardosantiago44@gmail.com>
2024-11-17 22:32:47 +0000

    Finished hash table implementation

--- HashTable/Cpp/main.cpp
+++ HashTable/Cpp/main.cpp
@@ -1,64 +1,64 @@
-#include "MyHashTable.h"
+#include <iostream>
+#include <vector>
+#include <algorithm>
 
+using namespace std;
 
-// Eliminar el main
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        int i = 0;
+        vector<vector<int>> result = {};
+
+        while (i < nums.size() && nums[i] <= 0) {
+            int left = i + 1;
+            int right = nums.size() - 1;
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+                
+                // Target found
+                if (sum == 0) {
+                    result.push_back({i, left, right});
+                } 
+
+                if (sum <= 0) {
+                    // Make the sum bigger by increasing left 
+                    left++;
+                    while (left < nums.size() && nums[left - 1] == nums[left]) {
+                        left++;
+                    }
+                } else {
+                    // Make the sum smaller by decreasing right
+                    right--;
+                    // Continue decreasing to avoid checking duplicates
+                    while (right > 0 && nums[right + 1] == nums[right]) {
+                        right--;
+                    }
+                }
+            }
+
+            i++;
+            while (nums[i - 1] == nums[i]) {
+                i++;
+            }
+        }
+
+        return result;
+    }
+};
+
 int main() {
-    MyHashTable table;
+    Solution s;
+    vector<int> nums = {0, 0, 0, 0};
+    vector<vector<int>> result = s.threeSum(nums);
 
-    table.put("Carlos", 42);
-    table.put("María", 18);
-    table.put("Juan", 25);
-    table.put("Lucía", 37);
-    table.put("Pedro", 60);
-    table.put("Ana", 22);
-    table.put("Luis", 34);
-    table.put("Elena", 29);
-    table.put("Jorge", 50);
-    table.put("Sofía", 33);
-    table.put("Miguel", 45);
-    table.put("Isabel", 19);
-    table.put("Raúl", 28);
-    table.put("Clara", 31);
-    table.put("Pablo", 40);
-    table.put("Laura", 26);
-    table.put("Daniel", 53);
-    table.put("Teresa", 47);
-    table.put("Álvaro", 38);
-    table.put("Carmen", 21);
-    table.put("Fernando", 49);
-    table.put("Paula", 27);
-    table.put("Hugo", 44);
-    table.put("Marta", 35);
-    table.put("Diego", 30);
-    table.put("Sara", 32);
-    table.put("Adrián", 41);
-    table.put("Nuria", 24);
-    table.put("Iván", 36);
-    table.put("Andrea", 23);
-
-    // Test get
-    try {
-        cout << "Andrea: " << table.get("Andrea") << endl;
-        cout << "Luis: " << table.get("Luis") << endl;
-        cout << "Daniel: " << table.get("Daniel") << endl;
-        cout << "Clara: " << table.get("Clara") << endl;
-        cout << "Diego: " << table.get("Diego") << endl;
-        cout << "Bernardo: " << table.get("Bernardo") << endl;
-    } catch (out_of_range e) {
-        cout << e.what() << endl;
+    for (int i = 0; i < result.size(); i++) {
+        for (int j = 0; j < result[i].size(); j++) {
+            cout << result[i][j] << " ";
+        }
+        cout << endl;
     }
-
-    try {
-        cout << "Jorge: " << table.get("Jorge") << endl;
-        table.remove("Jorge");
-        cout << "Jorge: " << table.get("Jorge") << endl;
-        table.remove("Luis");
-        table.remove("Daniel");
-        table.remove("Clara");
-        table.remove("Diego");
-        table.remove("Bernardo");
-    } catch (out_of_range e) {
-        cout << e.what() << endl;
-    }
-    table.put("Jorge", 23);
+    
+    return 0;
 }
